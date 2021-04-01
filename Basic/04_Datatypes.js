884809ffd46ecc31be947ef2125f93d5010edd04
mrhrifat <mrhrifat383759@gmail.com>
2021-04-02 05:12:04 +0600

    Update & Add

---
+++ Basic/04_Datatypes.js
@@ -0,0 +1,19 @@
+/*
+
+Mainly there is 2 kind of data types in javascript
+01) Primitive
+02) Obejct
+
+There is 5 kind of Primitive Data Types
+01) Number [Integer || 4 && Float || 4.0]
+02) String [In '' || "" || ``]
+03) Bollean [true || false]
+04) Undefined [Like something that not defined in code]
+05) Null [Like something intentional absence of any object value in code]
+
+There is 3 kind to Object Data Types
+01) Array
+02) Object
+03) Function
+
+*/
