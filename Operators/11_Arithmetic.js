58cf5abeaddc93d2919e52146a85e1a33d0293b5
mrhrifat <mrhrifat383759@gmail.com>
2021-04-02 20:48:55 +0600

    Arithmetic Operator

---
+++ Operators/11_Arithmetic.js
@@ -0,0 +1,60 @@
+/*Arithmetic Operator
+ +, -, *, /, % , ++ ,--
+ */
+var a = 30;
+var b = 5;
+
+//Summation
+var sum = a + b;
+console.log(sum);
+
+//Substraction
+var sub = a - b;
+console.log(sub);
+
+//Multiplication
+var mul = a * b;
+console.log(mul);
+
+//Division
+var div = a / b;
+console.log(div);
+
+//Modulus without reminder
+var modUnReminder = a % b;
+console.log(modUnReminder);
+
+//Modulus with reminder
+var b = 7
+var modReminder = a % b;
+console.log(modReminder);
+
+var c = 10;
+
+//Incremental Oparator
+
+//Pre Increment
+console.log(c)
+var preIncrement = ++c;
+console.log(c);
+
+var d = 15;
+//Post Increment
+console.log(d)
+var postIncrement = d++;
+console.log(d);
+
+//Decremental Oparator
+
+var e = 40;
+//Pre Decrement
+console.log(e)
+var preDecrement = --e;
+console.log(e);
+
+//Post Decrement
+
+var f = 50;
+console.log(f)
+var preDecrement = f--;
+console.log(f);
