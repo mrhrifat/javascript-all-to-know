63f3ca9ce6d2c350c5cb8748d905403d576c6a25
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-09-07 18:39:04 +0600

    Updating

--- OOP_Theory/107_Abstraction.js
+++
@@ -1,8 +0,0 @@
-/* 
-
-Abstraction
-01) It hide the implemention of core logic concept from the user.
-02) It provides the user only that part which are the method of object.
-03) The main goal of abstraction is hide complexity by hiding unnecessary details from the users.
-
-*/
--- OOP_Theory/108_Encapsulation.js
+++
@@ -1,6 +0,0 @@
-/* 
-
-Encapsulation
-01) It binding the property and method of an object like a capsule.
-
-*/
